89d4c6ba71e70d1f5df1487f5ca082ee824b2595
Swong87 <8swordz@gmail.com>
2017-10-02 17:41:45 -0400

    initial commit

---
+++ client/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const About = () =>
+    <div>
+      <div className="row">
+        <div className="text-center col-sm-12">
+          <p>Sign In</p>
+          <div className="g-signin2" data-onsuccess="onSignIn"></div>
+        </div>
+      </div>
+    </div>;
+
+export default About;
